32d608a1fae4560d5e6d3f5568d0089b654750a5
jackkershaw <hello@jackkershaw.net>
2025-02-24 12:49:25 +0000

    improve layout of feature card

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -32,20 +32,28 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
         title={feature.properties?.Title}
       />
 
-      <div className="mb-2 w-full px-2 py-4">
-        <h2 className="feature-card__title">{feature.properties?.Title}</h2>
-        <div className="feature-card__info">
-          <span>Designed by:</span> {feature.properties?.Designer}
-        </div>
-        <div className="mb-4 text-sm">
-          <span>Completed in:</span> {feature.properties?.Completed}
+      <div className="mb-2 w-full rounded-bl-lg rounded-br-lg bg-white px-2 py-4">
+        <h2 className="text-xl font-bold">{feature.properties?.Title}</h2>
+        <div className="flex flex-col space-y-3 py-4 text-base">
+          <div className="grid grid-cols-2 gap-2 px-4">
+            <span className="text-gray-800 font-bold">Designed by:</span>
+            <span className="text-gray-600 text-right">
+              {feature.properties?.Designer}
+            </span>
+          </div>
+          <div className="grid grid-cols-2 gap-2 px-4">
+            <span className="text-gray-800 font-bold">Completed in:</span>
+            <span className="text-gray-600 text-right">
+              {feature.properties?.Completed}
+            </span>
+          </div>
         </div>
         <div className="flex justify-between">
           <a
             href={feature.properties?.MapURL}
             target="_blank"
             rel="noopener noreferrer"
-            className="rounded-lg bg-white p-1 px-4 py-3 text-sm font-bold hover:bg-gray-600"
+            className="rounded-lg bg-gray/50 p-1 px-4 py-3 text-sm font-bold hover:bg-gray/100"
           >
             Get Directions
           </a>
@@ -56,14 +64,14 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
             }
             target="_blank"
             rel="noopener noreferrer"
-            className="rounded-lg bg-white p-1 px-4 py-3 text-sm font-bold hover:bg-gray-600"
+            className="rounded-lg bg-gray/50 p-1 px-4 py-3 text-sm font-bold hover:bg-gray/100"
           >
             More Info
           </a>
         </div>
       </div>
       <div
-        className="absolute right-2 top-2 flex h-20 w-20 flex-col items-center justify-start rounded-lg bg-white p-2"
+        className="absolute right-2 top-2 flex h-20 w-20 flex-col items-center justify-start rounded-lg bg-white p-2 hover:bg-gray"
         onClick={onVisitedToggle}
       >
         {isVisited ? <VisitedCheckbox /> : <UnvisitedCheckbox />}
@@ -72,7 +80,7 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
 
       {onClose && (
         <div
-          className="absolute left-2 top-2 flex h-20 w-20 flex-col items-center justify-start rounded-lg bg-white p-2"
+          className="absolute left-2 top-2 flex h-20 w-20 flex-col items-center justify-start rounded-lg bg-white p-2 hover:bg-gray"
           onClick={onClose}
         >
           <CloseButton />
