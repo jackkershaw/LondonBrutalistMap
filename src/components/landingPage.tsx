0d93cd179c00214d54769af5c9707ca8ee787b3f
jackkershaw <hello@jackkershaw.net>
2025-02-24 12:02:32 +0000

    improving design of landing page

--- src/components/landingPage.tsx
+++ src/components/landingPage.tsx
@@ -20,19 +20,17 @@ const LandingPage: React.FC = () => {
   }
 
   return (
-    <div className="landing-page fixed inset-0 z-10 flex h-screen items-center justify-center bg-black text-white">
+    <div className="fixed inset-0 z-10 flex h-screen items-center justify-center bg-black text-white">
       <div className="max-w-xl rounded-lg p-10 shadow-xl">
-        <h1 className="mb-6 mt-5 border-b-2 border-black py-2 pb-4 text-4xl font-bold">
+        <h1 className="mt-5 border-b-2 border-black py-2 text-4xl font-bold">
           Welcome to the London Brutalist Map!
         </h1>
-        <p>
+        <p className="pb-2">
           This site serves as a guide to London's Brutalist architecture,
           allowing you to learn about, and track visits to various Brutalist
           buildings.
         </p>
-        <h2 py-2 mt-1 text-2xl font-bold>
-          Explore the map:
-        </h2>
+        <h2 className="py-2 text-2xl font-bold">Explore the map:</h2>
         <div>
           <ul className="list-disc pl-5">
             <li className="p-1">
@@ -45,20 +43,11 @@ const LandingPage: React.FC = () => {
             <li>Unvisited buildings will be red, visited ones are green.</li>
           </ul>
         </div>
-        <h2>View Buildings:</h2>
-        <ul className="list-disc pl-5">
-          <li className="p-1">
-            View all the buildings in a list format. You can access this by
-            clicking on the visited count.
-          </li>
-        </ul>
-        <h2 py-2 font-bold mt-1 text-2xl>
-          See Your Visited Buildings:
-        </h2>
+        <h2 className="py-2 text-2xl font-bold">See Your Visited Buildings:</h2>
         <ul className="list-disc pl-5">
           <li className="p-1">
             At the bottom of the page you can find a visited count, showing how
-            many of the 104 buildings you've marked as visited.
+            many of the 104 buildings you've visited.
           </li>
         </ul>
         <button
