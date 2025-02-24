5e04568c1ef30b7f0a52225e9f8d4efe0159f162
jackkershaw <hello@jackkershaw.net>
2025-02-24 14:03:36 +0000

    fixed syntax errors

--- src/pages/about.tsx
+++ src/pages/about.tsx
@@ -1,32 +1,32 @@
 export default function AboutPage() {
   return (
     <div>
-      <div className="space-y-5 mx-5 py-10 sm:mx-0 grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-16">
+      <div className="mx-5 grid grid-cols-1 gap-16 space-y-5 py-10 sm:mx-0 md:grid-cols-2 lg:grid-cols-3">
         <div className="col-span-1">
           <section>
             <h1 className="text-2xl">What is Brutalism?</h1>
-            <p className="text-xl py-4">
-              Brutalism is an architectural style that emerged in the
-              1950s. It is characterized by its use of raw, unfinished
+            <p className="py-4 text-xl">
+              Brutalism is an architectural style that emerged in the 1950s. It
+              1950s. It is characterized by its use of raw, unfinished concrete,
               concrete, bold geometric shapes, and an expressive design
-              approach. The term "brutalism" originates from Le
-              Corbusier's phrase in French: "béton brut," which means
-              "raw concrete."
+              approach. The term "brutalism" originates from Le Corbusier's
+              Corbusier's phrase in French: "béton brut," which means "raw
+              concrete."
             </p>
           </section>
           <section>
             <h1 className="text-2xl font-bold">Why London?</h1>
-            <p className="text-xl py-4">
-              London has some of the best examples of Brutalism in the
-              world. After World War II, a group of idealists began to
-              rebuild the city in concrete. What remain are some of the
-              most striking civic and housing projects ever built.
+            <p className="py-4 text-xl">
+              London has some of the best examples of Brutalism in the world.
+              After World War II, a group of idealists began to rebuild the city
+              in concrete. What remain are some of the most striking civic and
+              housing projects ever built.
             </p>
           </section>
           <section>
-            <p className="text-xl py-10">
-              This website was created by Jack Kershaw and the code can
-              be viewed{" "}
+            <p className="py-10 text-xl">
+              This website was created by Jack Kershaw and the code can be
+              viewed{" "}
               <a
                 href="https://github.com/jackkershaw/brutalist-buildings-guesser"
                 className="underline"
@@ -50,7 +50,7 @@ export default function AboutPage() {
         <img
           src="/images/AboutPage.webp"
           alt="building"
-          className="filter grayscale w-full h-[70vh] object-contain col-span-1 md:col-start-2 lg:col-start-3"
+          className="col-span-1 h-[70vh] w-full object-contain grayscale filter md:col-start-2 lg:col-start-3"
         />
       </div>
     </div>
