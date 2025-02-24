0f9da7a6100d92f6f322df64e5543fcf7bdf0ec3
jackkershaw <hello@jackkershaw.net>
2025-02-24 15:46:03 +0000

    improve layout of feature card so more consistent

--- src/components/FeatureCard.tsx
+++ src/components/FeatureCard.tsx
@@ -20,40 +20,39 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
   if (!isOpen) return null;
 
   return (
-    <div className="relative flex h-[70vh] flex-col items-center justify-center overflow-hidden sm:h-[65vh] sm:rounded-lg">
-      <img
-        src={`./images/buildings/${feature.properties?.Image}.webp`}
-        alt={feature.properties?.Title}
-        width={400}
-        height={400}
-        sizes="100vw"
-        className="h-[45vh] w-full object-cover object-center grayscale filter"
-        loading="eager"
-        title={feature.properties?.Title}
-      />
+    <div className="relative flex h-[500px] flex-col overflow-hidden border-2 border-black bg-white sm:rounded-lg">
+      <div className="h-[300px] w-full">
+        <img
+          src={`./images/buildings/${feature.properties?.Image}.webp`}
+          alt={feature.properties?.Title}
+          width={400}
+          height={400}
+          sizes="100vw"
+          className="h-full w-full object-cover object-center grayscale filter"
+          loading="eager"
+          title={feature.properties?.Title}
+        />
+      </div>
 
-      <div className="mb-2 w-full rounded-bl-lg rounded-br-lg bg-white px-2 py-4">
-        <h2 className="px-4 text-xl font-bold">{feature.properties?.Title}</h2>
-        <div className="flex flex-col space-y-3 py-4 text-base">
-          <div className="grid grid-cols-2 gap-2 px-4">
-            <span className="text-gray-800 font-bold">Designed by:</span>
-            <span className="text-gray-600 text-right">
-              {feature.properties?.Designer}
-            </span>
-          </div>
-          <div className="grid grid-cols-2 gap-2 px-4">
-            <span className="text-gray-800 font-bold">Completed in:</span>
-            <span className="text-gray-600 text-right">
-              {feature.properties?.Completed}
-            </span>
+      <div className="flex w-full flex-1 flex-col justify-between p-4">
+        <div>
+          <h2 className="mb-4 text-xl font-bold">
+            {feature.properties?.Title}
+          </h2>
+          <div className="grid grid-cols-2 gap-2 text-base">
+            <span className="font-bold">Designed by:</span>
+            <span className="text-right">{feature.properties?.Designer}</span>
+            <span className="font-bold">Completed in:</span>
+            <span className="text-right">{feature.properties?.Completed}</span>
           </div>
         </div>
-        <div className="flex justify-between">
+
+        <div className="flex justify-between gap-4">
           <a
             href={feature.properties?.MapURL}
             target="_blank"
             rel="noopener noreferrer"
-            className="rounded-lg bg-gray/50 p-1 px-4 py-3 text-sm font-bold hover:bg-gray/100"
+            className="rounded-lg bg-gray/50 px-4 py-2 text-sm font-bold hover:bg-gray/100"
           >
             Get Directions
           </a>
@@ -64,12 +63,13 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
             }
             target="_blank"
             rel="noopener noreferrer"
-            className="rounded-lg bg-gray/50 p-1 px-4 py-3 text-sm font-bold hover:bg-gray/100"
+            className="rounded-lg bg-gray/50 px-4 py-2 text-sm font-bold hover:bg-gray/100"
           >
             More Info
           </a>
         </div>
       </div>
+
       <div
         className="absolute right-2 top-2 flex h-20 w-20 flex-col items-center justify-start rounded-lg bg-white p-2 hover:bg-gray"
         onClick={onVisitedToggle}
@@ -90,4 +90,5 @@ const FeatureCard: React.FC<FeatureCardProps> = ({
     </div>
   );
 };
+
 export default FeatureCard;
